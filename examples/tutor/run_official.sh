6ad7d19274a6717f9aaab1205e4952c1e295f214
HZxCzar <hz.czar@sjtu.edu.cn>
2026-05-11 02:25:51 +0800

    fix run

--- examples/tutor/run_official.sh
+++ examples/tutor/run_official.sh
@@ -10,7 +10,6 @@ export TRANSFORMERS_OFFLINE="${TRANSFORMERS_OFFLINE:-1}"
 export HF_DATASETS_OFFLINE="${HF_DATASETS_OFFLINE:-1}"
 export WANDB_MODE="${WANDB_MODE:-offline}"
 
-TRIAL_NAME="${TRIAL_NAME:-$(date +%Y%m%d_%H%M%S)}"
 EXPERIMENT_NAME="${EXPERIMENT_NAME:-tutor}"
 N_GPUS_PER_NODE="${N_GPUS_PER_NODE:-4}"
 TUTOR_DATASET_PATH="$ROOT_DIR/examples/tutor/aime_dataset_no_pre_solve"
@@ -20,7 +19,5 @@ python examples/tutor/train.py \
   scheduler.type=local \
   cluster.n_gpus_per_node="$N_GPUS_PER_NODE" \
   experiment_name="$EXPERIMENT_NAME" \
-  trial_name="$TRIAL_NAME" \
   train_dataset.path="$TUTOR_DATASET_PATH" \
-  valid_dataset.path="$TUTOR_DATASET_PATH" \
-  "$@"
+  valid_dataset.path="$TUTOR_DATASET_PATH"
